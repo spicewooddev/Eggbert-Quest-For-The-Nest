a8d4fed48f4597f5aed48693e8fb6eec9d4f93d3
spicewooddev <shloth03@gmail.com>
2024-10-03 15:34:14 -0500

    Made jump work properly, and added custom gravity scale, changed player color to green,
    im still feeling sick, but it's just been a runny nose right now. after this, we should focus on making a little area with a wide variety of platforms for the player to jump around on and have the camera follow the player. that way, we can think of how best to approach the game feel of this (like how moving and jumping feels) and make changes to that. because if we try to change that stuff later, it will be a lot harder and we'll end up having to rework levels entirely.

--- Eggbert - Quest for The Nest!/Assets/Scripts/Eggbert.cs
+++ Eggbert - Quest for The Nest!/Assets/Scripts/Eggbert.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Eggbert : MonoBehaviour
 {
-    private float speed = 10;
     private Rigidbody2D body;
+
+    [SerializeField] float moveSpeed = 10;
+
+    private float jumpHeight = 5;
+    [SerializeField] float gravityScale = 15;
+    [SerializeField] float fallGravityScale = 20;
+
+    bool isGrounded = false;
 
     // Start is called before the first frame update
     void Start()
@@ -13,15 +21,37 @@ public class Eggbert : MonoBehaviour
         body = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    //For physics, we want to use FixedUpdate() instead of Update().
+    void FixedUpdate()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        //By using GetAxisRaw, the player will hard stop when they release the movement keys, providing them more control
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        body.velocity = new Vector2(horizontalInput * moveSpeed, body.velocity.y);
   
-        if (Input.GetKey("space"))
+        if (Input.GetKey("space") && isGrounded)
         {
-            body.velocity = new Vector2(body.velocity.x, speed);
+            body.gravityScale = gravityScale;
+            float jumpForce = Mathf.Sqrt(jumpHeight * (Physics2D.gravity.y * body.gravityScale) * -2) * body.mass;
+            body.velocity = new Vector2(body.velocity.x, jumpForce);
         }
+
+        if (body.velocity.y > 0)
+        {
+            body.gravityScale = gravityScale;
+        }
+        else
+        {
+            body.gravityScale = fallGravityScale;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
     }
 }
